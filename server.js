10c6a210fd21bfa603199625d06a747e4c0150d0
dharaneshk2003 <126548295+dharaneshk2003@users.noreply.github.com>
2025-01-11 12:47:11 +0530

    Update server.js/test[home]

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(express.json());
 
 // Routes
+
+app.get("/", (req, res) => {
+    res.send("Welcome");
+});
+
 app.use('/api', cryptoRoutes);
 
 // Start Scheduler
